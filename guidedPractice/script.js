540dc4e2bda73e75e900f1ae24f5d56914cb86f1
DebFullstack <debbieking1979@gmail.com>
2023-05-15 20:13:52 -0400

    added and centered img

---
+++ guidedPractice/script.js
@@ -0,0 +1,11 @@
+const root = document.getElementById ("root")
+const h1 = document.createElement ("h1")
+h1.innerHTML = "Mona Lisa"
+root.appendChild (h1)
+
+const monaImg = document.createElement ("img")
+monaImg.setAttribute("src", "img/mona_lisa.jpg");
+root.appendChild (monaImg)
+monaImg.setAttribute("width", "304");
+monaImg.setAttribute("height", "auto");
+root.style.textAlign = "center";
