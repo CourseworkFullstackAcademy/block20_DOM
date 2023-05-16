2e368a95b4083c923b7b5ab887280110629b29e7
DebFullstack <debbieking1979@gmail.com>
2023-05-15 20:17:06 -0400

    added p text

--- guidedPractice/script.js
+++ guidedPractice/script.js
@@ -4,8 +4,12 @@ h1.innerHTML = "Mona Lisa"
 root.appendChild (h1)
 
 const monaImg = document.createElement ("img")
-monaImg.setAttribute("src", "img/mona_lisa.jpg");
+monaImg.setAttribute("src", "img/mona_lisa.jpg")
 root.appendChild (monaImg)
-monaImg.setAttribute("width", "304");
-monaImg.setAttribute("height", "auto");
-root.style.textAlign = "center";
+monaImg.setAttribute("width", "304")
+monaImg.setAttribute("height", "auto")
+root.style.textAlign = "center"
+
+const pTag = document.createElement ("p")
+pTag.innerHTML = "The Mona Lisa is a famous painting."
+root.appendChild (pTag)
